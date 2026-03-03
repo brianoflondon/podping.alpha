6b1b2899ee08212efe98d1d8fcefea3f29600514
Dave Jones <sect62@gmail.com>
2026-03-03 07:00:28 -0600

    docker hub repo names

--- docker/multi-arch-builder.sh
+++ docker/multi-arch-builder.sh
@@ -4,4 +4,4 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx rm builder
 docker buildx create --name builder --driver docker-container --use
 docker buildx inspect --bootstrap
-sudo docker buildx build --platform linux/amd64 --tag podcastindexorg/podcasting20-podping.alpha/frontend:3.0.0 --tag podcastindexorg/podcasting20-podping.alpha/frontend:latest --no-cache --output "type=registry" .
+sudo docker buildx build --platform linux/amd64 --tag podcastindexorg/podcasting20-podping.alpha:3.0.0 --tag podcastindexorg/podcasting20-podping.alpha:latest --no-cache --output "type=registry" .
